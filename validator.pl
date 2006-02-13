322deab6656e8a888a80ea7bf8db92d4deb4e21c
Roel de Cock <roel@dtu.dk>
2006-02-13 13:25:04 +0000

    Now validates files hundred at 1 command line, which only takes a few minutes on 20000 files.

--- validator.pl
+++ validator.pl
@@ -10,17 +10,27 @@ use Data::Dumper;
 use POSIX qw(floor ceil);
 
 my @files = <>;
-for(my $i= 0; $ < $#files; $i++) {
+for(my $i= 0; $i <= $#files; $i++) {
   chomp $files[$i];
 }
 my $chunks = ceil(@files / $chunksize);
-while(my $file = <>) {
-  chomp $file;
-  if(my @err = check($file)) {
-    print "---$file\n";
-    print join("\n", @err, "\n");
-  } else {
-    print "+++$file\n";
+
+for(my $i=0; $i < $chunks; $i++) {
+  my %ret;
+  my $base = $i * $chunksize;
+  # prevent reaching over the top, will cause undefs in chunking
+  my $top = $i == $chunks-1 ? $#files : $base+$chunksize+1;
+  my @batch = @files[$base .. $top];
+  check(\%ret, @batch);
+
+  print STDERR "Done chunk ", $i+1, " of $chunks\n";
+  # Only problem with hashes is, the order get mangled.
+  foreach my $fn (@batch) {
+    next unless exists $ret{$fn};
+    print "$fn\n";
+    foreach my $err (@{$ret{$fn}}) {
+      print "  $err\n";
+    }
   }
 }
 
@@ -28,14 +38,14 @@ while(my $file = <>) {
 sub check
 {
   my($hash, @files) = @_;
-
   my $args = join(' ', @files);
+
   my @res = `$xmllint --noout --schema $schema $args 2>&1`;
   while(my $l = shift @res) {
     chomp $l;
     # ignore bogus xs:language errors for 3-char languages
     next unless $l =~ /validity error/i && $l !~ /not a valid value of the atomic type 'xs:language'/i;
-    my($file) = $l =~ /^(.+): /;
+    my($file) = $l =~ /^(.+?):/;
     push(@{$hash->{$file}}, $l);
   }
   return $hash;
