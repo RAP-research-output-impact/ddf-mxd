448075ec8ed86806c52304cac871987fd21a0eb8
Roel de Cock <roel@dtu.dk>
2009-02-19 21:59:22 +0000

    Q&D script to generate/update the files in /var/www/orbit2mxd/.../abs/

---
+++ genabs.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use Cwd;
+use File::Basename;
+
+open(IN, '<orb2mxd.xsl') or die "$!";
+open(OUT, '>abs/orb2mxd.xsl') or die "$!";
+
+my $here = getcwd();
+$here =~ s|^/var/www/||;
+my $absurl = "http://$here/abs/";
+print STDERR "set absurl=$absurl\n";
+
+while(<IN>) {
+  s/\>\</\>$absurl\</ if /variable name="baseurl"/;
+  print OUT;
+}
+close(IN); close(OUT);
+
+system ("rsync -vu --existing *.xsl *.xml abs/");
